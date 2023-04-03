e4c4cdd6d8b84459e43df7693628c40538fd05df
Kuba162003 <126468460+Kuba162003@users.noreply.github.com>
2023-04-03 11:52:11 +0200

    Add files via upload

---
+++ lesson-04/lab4/lab4.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<string>
+
+class Wire {
+private:
+	double _long;
+public:
+	double Length() { return this->_long; };
+	Wire() { this->_long = 0.0; };
+	Wire(double size) { this->_long = size; };
+	Wire(float size) : Wire(static_cast<double>(size)) {};
+	Wire(int size) : Wire(static_cast<double>(size)) {};
+	Wire(std::string size) : Wire(std::stod(size)) {};
+};
+
+int main() {
+	Wire defaultSize;
+	Wire doubleSize(25.76);
+	Wire floatSize(292.01f);
+	Wire stringSize("44.87");
+	Wire intSize(560);
+	std::cout << defaultSize.Length() << std::endl;
+	std::cout << doubleSize.Length() << std::endl;
+	std::cout << floatSize.Length() << std::endl;
+	std::cout << stringSize.Length() << std::endl;
+	std::cout << intSize.Length() << std::endl;
+	std::cout << "typeid of stringSize.Length() -> " << typeid(stringSize.Length()).name();
+	return 0;
+}
