0ca7ff9eade2fe6ec8f2e75393112f7b2a7ea76a
Kuba162003 <126468460+Kuba162003@users.noreply.github.com>
2023-04-03 12:40:39 +0200

    Add files via upload

---
+++ lesson-05/lab5/lab5.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+
+class Length {
+public:
+	Length() {};
+	virtual ~Length() {}
+	virtual Length* AddLength(float piece) = 0;
+	virtual std::string Value() = 0;
+};
+
+class Tape : public Length {
+private:
+	float centimeters;
+public:
+	Tape* AddLength(float piece){ 
+		return new Tape(centimeters + piece); 
+	}
+	Tape* Cut(float piece) {
+		return new Tape(centimeters - piece);
+	}
+	Tape* CalculateToInches(){
+		return new Tape(centimeters / 2.54);
+	}
+	std::string Value(){ 
+		return "cm"; 
+	}
+	Tape(float centimeters){ 
+		this->centimeters = centimeters; 
+	}
+};
+
+int main() {
+	Tape myTape(8);
+	Tape* biggerTape = myTape.AddLength(10);
+	Tape* smollerTape = myTape.Cut(4);
+	Tape* TypeInInches = myTape.CalculateToInches();
+}
