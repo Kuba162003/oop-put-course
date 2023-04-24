b5de91e4ddab069d3df75ea40eaaf83cd527fe46
Kuba162003 <126468460+Kuba162003@users.noreply.github.com>
2023-04-24 13:09:59 +0200

    Add files via upload

---
+++ lesson-06/lab6/lab6.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <memory>
+#include<sstream>
+
+class Sequence {
+public:
+	virtual int Length() = 0;
+};
+
+class FakeSequence : public Sequence {
+public:
+	int Length() override { return 100; }
+};
+
+class Gene {
+private:
+	std::string name_;
+	std::shared_ptr<Sequence> sequence_;
+public:
+	Gene(std::string name, std::shared_ptr<Sequence> sequence);
+	sequence_(sequence), name_(name) {};
+	std::string JSON() {
+		std::stringstream ss;
+		ss << "{" << "\"name\":\"" <<name_ <<"\"length\": " << sequence_->Length() << "}"
+	}
+};
+int main()
+{
+	std::shared_ptr<Sequence> fake_seq = std::make_shared<FakeSequence>();
+	std:cout << gene.JSON() << std::endl;
+	assert(fake_seq->Length == 100);
+	return 0;
+}
